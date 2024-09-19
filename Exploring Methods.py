5f1b7dd1d64da4962b43b58eb048878250a50c4d
LaibaRazi <khanlayba72@gmail.com>
2024-09-20 03:55:10 +0500

    operators with strings

--- Exploring Methods.py
+++ Exploring Methods.py
@@ -9,4 +9,16 @@ import pandas
 print("hi");
 print(6*(2+1)); #first it will do add and then multiple
 print((6)*2+1); #first multiple and then it's gonna
-print("I\'m Laiba razi Khan\n and I'm a coding Instructor, I love \"scratch\" as well")
+print("I\'m Laiba razi Khan\n and I'm a coding Instructor, I love \"scratch\" as well");
+
+# dealing with operators 
+print(str(2+6)+" Add Op") #add operator
+print(str(2-7)+" Subtract") #subtract
+print(str(6**9)+" Exponential") #exponential
+print(str(6%6)+" Modulus") #modulus
+
+#str is used to first perform the operation and then convert to string
+
+print("First Putting the Text"+str(2+8))
+# print("trying divide this time"+7/2) this is wrong you should write it as str first
+print("This time divide with str"+str(2/8))
