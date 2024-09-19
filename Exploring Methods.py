d49d4e4ec96ff3664cc8b44f2085e6f7c0ade79c
LaibaRazi <khanlayba72@gmail.com>
2024-09-20 03:27:50 +0500

    this is updated

---
+++ Exploring Methods.py
@@ -0,0 +1,12 @@
+# first file
+"""
+this is a
+multi line comment
+"""
+
+
+import pandas
+print("hi");
+print(6*(2+1)); #first it will do add and then multiple
+print((6)*2+1); #first multiple and then it's gonna
+print("I\'m Laiba razi Khan\n and I'm a coding Instructor, I love \"scratch\" as well")
