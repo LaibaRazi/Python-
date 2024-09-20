dba4fa2343aec92203513685d2442a2a986c8f96
LaibaRazi <khanlayba72@gmail.com>
2024-09-21 03:38:36 +0500

    strings and input with type casting

---
+++ Input&Strings.py
@@ -0,0 +1,38 @@
+#by default input is a string
+firstnumber=input("Enter your first number ")
+secondnumber= input("Enter your second number ")
+
+print("***************************")
+print(firstnumber + secondnumber) # It will concat the  answers only
+firstnumber=int(input("Enter your first number "))
+secondnumber= int(input("Enter your second number "))
+
+print("***************************")
+print(firstnumber + secondnumber) # this one will sum the numbers up
+print(str(firstnumber+secondnumber)+ " This print is with some text that's why we have to use str with it")
+
+"""
+Now using the operators with the input how does it responds
+"""
+#default input 
+
+print("***************************")
+ADDnum1=input("Enter your first number for addition ")
+ADDnum2=input("Enter your Second number for addition ")
+
+#print 
+print("***************************")
+print(str(ADDnum1+ADDnum2)+" working as a concat ")
+print(int(ADDnum1)+int(ADDnum2));
+
+#let's see triple quotes of string
+string_one_with_triple_quotes="""I'm writing this string in order to know if it can handle this much text and I know this is not a 
+huge text but due to big in nature so python triple qoutes can handle this & yes one more thing we can do it with single qoutes as welll please
+try on your own as well"""
+
+string_two_with_single_triple_quotes='''I'm writing this string in order to know if it can handle this much text and I know this is not a 
+huge text but due to big in "nature"(single quotes allow double quotes in it) so python triple qoutes can handle this & yes one more thing we can do it with single qoutes as welll please
+try on your own as well'''
+
+for character in string_one_with_triple_quotes:
+    print(character)
---
+++ TypeCasting&ExploringMethods.py
@@ -0,0 +1,31 @@
+#type casting means conversion of one datatype into another:
+#python also supports wide variety of methods and functions:
+
+# list of methods are here
+"""
+int()
+float()
+ord()
+hex()
+oct()
+tuple()
+set()
+list()
+dict() these methods are there to typecast in python
+    
+"""
+print("let's work on typecasting")
+Checknum1="2"
+Checknum2="7"
+print(int(Checknum1)+int(Checknum2)) #answer is 9
+
+#implicit conversion - automatically done
+#explicit -done with methods 
+
+#EX 2
+
+Checknum3="4"
+Checknum4=6
+print(int(Checknum3)+Checknum4)#answer is 10
+
+
